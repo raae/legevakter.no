414aff3af2d9f134cd8cb6b9ed7732762b71d89a
Benedicte Raae <raae@bgraphic.no>
2018-03-09 21:01:26 +0100

    Created real implementation of parse opening hours helper with tests.

--- src/helpers/health-service.js
+++ src/helpers/health-service.js
@@ -1,6 +1,12 @@
-function parseOpeningHours(hours) {
-  return hours;
-}
-module.exports = {
-  parseOpeningHours
+import { startOfWeek, addMinutes } from "date-fns";
+
+export const parseOpeningHours = (openingHours, today) => {
+  const baseline = startOfWeek(today, { weekStartsOn: 1 });
+  return openingHours
+    .split(",")
+    .map(part => part.split("-"))
+    .map(([opening, closing]) => ({
+      opening: addMinutes(baseline, opening),
+      closing: addMinutes(baseline, closing)
+    }));
 };
--- src/helpers/health-service.test.js
+++ src/helpers/health-service.test.js
@@ -1,5 +1,65 @@
-const { parseOpeningHours } = require("./health-service");
+import { parseOpeningHours } from "./health-service";
 
-test("return input", () => {
-  expect(parseOpeningHours("input")).toBe("input");
+describe("Helpers for Health Service Data", () => {
+  describe("#parseOpeningHours", () => {
+    test("DISTR LEGEN I ROLLAG", () => {
+      expect(
+        parseOpeningHours(
+          "540-900,1980-2340,3420-3780,4860-5220,6300-6660",
+          new Date(2018, 2, 1)
+        )
+      ).toEqual([
+        {
+          opening: new Date(2018, 1, 26, 9),
+          closing: new Date(2018, 1, 26, 15)
+        },
+        {
+          opening: new Date(2018, 1, 27, 9),
+          closing: new Date(2018, 1, 27, 15)
+        },
+        {
+          opening: new Date(2018, 1, 28, 9),
+          closing: new Date(2018, 1, 28, 15)
+        },
+        {
+          opening: new Date(2018, 2, 1, 9),
+          closing: new Date(2018, 2, 1, 15)
+        },
+        {
+          opening: new Date(2018, 2, 2, 9),
+          closing: new Date(2018, 2, 2, 15)
+        }
+      ]);
+    });
+
+    test("LEGETJENESTEN I TYNSET", () => {
+      expect(
+        parseOpeningHours(
+          "930-1920,2370-3360,3810-4800,5250-6240,6690-10560",
+          new Date(2018, 2, 1)
+        )
+      ).toEqual([
+        {
+          opening: new Date(2018, 1, 26, 15, 30),
+          closing: new Date(2018, 1, 27, 8)
+        },
+        {
+          opening: new Date(2018, 1, 27, 15, 30),
+          closing: new Date(2018, 1, 28, 8)
+        },
+        {
+          opening: new Date(2018, 1, 28, 15, 30),
+          closing: new Date(2018, 2, 1, 8)
+        },
+        {
+          opening: new Date(2018, 2, 1, 15, 30),
+          closing: new Date(2018, 2, 2, 8)
+        },
+        {
+          opening: new Date(2018, 2, 2, 15, 30),
+          closing: new Date(2018, 2, 5, 8)
+        }
+      ]);
+    });
+  });
 });
