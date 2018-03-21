4a5ceb1751c9b1384b5135af86a7ef63dd3a614b
Benedicte Raae <raae@bgraphic.no>
2018-03-21 13:14:28 +0100

    Cleaned up code.

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,24 +3,11 @@ import Link from "gatsby-link";
 import HealthService from "../components/HealthService";
 
 const AppPage = ({ data }) => {
-  console.log(data);
-  console.log(data.allHealthService);
   return (
     <div>
       {data.allHealthService.edges.map(({ node }, index) => (
         <HealthService key={index} {...node} />
       ))}
-      {/* {data.allFile.edges.map(({ node }, index) => (
-      <tr key={index}>
-        <td>{node.relativePath}</td>
-        <td>{node.prettySize}</td>
-        <td>{node.extension}</td>
-        <td>{node.birthTime}</td>
-      </tr>
-    ))}
-    {data.allHealthService.each(healthServie => {
-      <div>{healthServie.name}</div>;
-    })} */}
     </div>
   );
 };
